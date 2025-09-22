28adb5477bd18827c522da5c4465c8dd6741ffc0
DanBanTUP <120801914+DanBanTUP@users.noreply.github.com>
2025-09-22 20:50:21 +0300

    Update task_021.py

--- task_021.py
+++ task_021.py
@@ -2,32 +2,35 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-#Читаем данные из JSON файла
 
+# Читаем данные из JSON файла
 with open(sys.argv[1], 'r') as file:
     data = json.load(file)
-
 x_values = []
 y_values = []
 
-#Извлекаем данные из JSON
+# Извлекаем данные из JSON
 for point in data['data']:
     x_values.append(point['x'])
     y_values.append(point['y'])
 
-#Строим график
+# Строим график
 graf_from_json = plt.subplot()
 plt.plot(x_values, y_values)
 plt.xlabel('x')
 plt.ylabel('y')
 graf_from_json.set_xlim((-5, 5))
 plt.title('Graph of y = f(x)')
-def set_yticks_step():
-    if(len(sys.argv) > 2)and((sys.argv[2]) == "Y"):
-        step = float(sys.argv[3])
-    else:
-        step = 1
-    plt.yticks(np.arange(min(y_values), max(y_values)+1, step))
 
-set_yticks_step()
+# Определяем шаг рисок по оси Y из командной строки
+if len(sys.argv) > 2:
+        try:
+            y_step = float(sys.argv[2])
+            plt.yticks(np.arange(min(y_values), max(y_values) + 1, y_step))
+        except ValueError:
+            print("Ошибка: Некорректный ввод шага для оси Y.")
+else:
+    y_step = 1  # Значение по умолчанию
+
+# Отображаем график
 plt.show()
